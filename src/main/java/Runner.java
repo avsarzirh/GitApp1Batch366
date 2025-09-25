0cf9e9f4a1e435ec88b5788693e6936adf04fae8
Avşar Zırh <zirhavsar@icolud.com>
2025-09-25 22:18:52 +0300

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        //Version 1
+        System.out.println("Hello GIT");
+        System.out.println("git add . : Stating area'ya alır.");
+    }
+}
